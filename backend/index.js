07c0baa18835195814763005aac04f73dcd6746d
Udgeet <bhatt8udgeet@gmail.com>
2024-04-07 16:26:56 +0530

    "files"

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const { createTodo } = require("./types");
+const app = express();
+
+
+app.use(express.json());
+
+//body {
+  //title: string;
+  //description: string;
+//}
+
+app.post("/todo", function(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+      res.status(411).json({
+        msg: "You sent the wrong inputs",
+      })
+      return;
+    }
+    // put it in mongodb
+});
+
+app.get('/todos', function(req,res) {
+
+})
+
+app.put('/completed', function(req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+       msg: "You sent the wrong inputs",
+    })
+    return;
+  }
+})
+
+//write basic express boilerplate code
+// with express.json() middleware
+
+const PORT = 8000;
